654e2ab5481f2f52cd65c79f5b14f68d5d1389d4
vitordelfino <vitor.delfino952@gmail.com>
2017-06-14 18:36:01 -0300

    tint status bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,8 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      statusBar.overlaysWebView(true);
+      statusBar.backgroundColorByHexString('#488aff');
       splashScreen.hide();
     });
   }
